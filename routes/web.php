6e61860e5b6fc682ec3d8e519b4287b3299877da
Mehedi Hasan <77448606+fnnaeem1881@users.noreply.github.com>
2024-04-09 10:17:40 +0600

    first commit

---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\Payment\StripePaymentController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/contact-us', [HomeController::class, 'contact_us'])->name('contact_us');
+Route::get('/about-us', [HomeController::class, 'about_us'])->name('about_us');
+Route::get('/stripe', [StripePaymentController::class, 'stripe']);
+Route::post('/stripe', [StripePaymentController::class, 'stripePost'])->name('stripe.post');
